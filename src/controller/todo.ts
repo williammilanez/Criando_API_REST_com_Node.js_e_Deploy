00a239790245c34755b060abf8891149c7606cf8
William Milanez <william.milanez@outlook.com>
2026-02-19 15:43:31 -0300

    Criação de uma nova tarefa implementando o método create e configurando a rota correspondente. Abordando a validação dos parâmetros, onde o título é obrigatório e a descrição opcional. Utilizamos o Prisma para interagir com o banco de dados, garantindo que as tarefas sejam criadas corretamente. Testando a funcionalidade com o Insomnia, confirmando que as tarefas estão sendo listadas.

--- src/controller/todo.ts
+++ src/controller/todo.ts
@@ -19,4 +19,26 @@ export class TodoController {
       return res.status(500).json({ error: "Erro ao listar tarefas" });
     }
   }
+
+  async createTodo(req: Request, res: Response) {
+    try {
+      const { title, description } = req.body;
+
+      if (!title) {
+        return res.status(400).json({ error: "O título é obrigatório" });
+      }
+
+      const todo = await prisma.todo.create({
+        data: {
+          title,
+          description,
+        },
+      });
+
+      return res.status(201).json(todo);
+    } catch (error) {
+      console.error("Erro ao criar a tarefa:", error);
+      return res.status(500).json({ error: "Erro ao criar tarefa" });
+    }
+  }
 }
--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -9,4 +9,8 @@ routerTodo.get("/api/todos", (req, res) =>
   todoController.getAllTodos(req, res),
 );
 
+routerTodo.post("/api/todos", (req, res) =>
+  todoController.createTodo(req, res),
+);
+
 export default routerTodo;
